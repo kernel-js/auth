ce2faba5b274bac3d76838fb66472c95aa2b9820
Bruno Santos <bruno.dos1997@gmail.com>
2019-04-22 17:57:36 -0300

    remove dist and node_modules

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import StorageMethods from './enums/StorageMethods';
-import OAuthAuthentication from './authorization/OAuthAuthentication';
-import AuthenticationInterface from './interfaces/AuthenticationInterface';
-export { StorageMethods, OAuthAuthentication, AuthenticationInterface, };
